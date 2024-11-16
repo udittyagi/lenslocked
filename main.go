6334204f8c22e3a438663e27e22457eaa64eed4b
Udit Tyagi <udittyagi@Udits-MacBook-Pro.local>
2024-11-16 23:04:44 +0530

    faaq via template

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func executeTemplate(w http.ResponseWriter, filePath string) {
 		return
 	}
 
-	err = tpl.Execute(w, "a string")
+	err = tpl.Execute(w, nil)
 	if err != nil {
 		log.Printf("Error while Executing Template: %v", err)
 		http.Error(w, "Error While Executing Template", http.StatusInternalServerError)
@@ -38,24 +38,7 @@ func contacthandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, `<h1>FAQ Page</h1>
-  <ul>
-	<li>
-	  <b>Is there a free version?</b>
-	  Yes! We offer a free trial for 30 days on any paid plans.
-	</li>
-	<li>
-	  <b>What are your support hours?</b>
-	  We have support staff answering emails 24/7, though response
-	  times may be a bit slower on weekends.
-	</li>
-	<li>
-	  <b>How do I contact support?</b>
-	  Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
-	</li>
-  </ul>
-  `)
+	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
 }
 
 func galleryHandler(w http.ResponseWriter, r *http.Request) {
